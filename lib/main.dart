c35821be29e46194fc32797c4bf19823c1e00d1d
Tashinamgyel <tashin599@gmail.com>
2026-05-28 12:20:34 +0600

    feat: No Screen Shot, Overlay Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:no_screenshot/no_screenshot.dart';
 import 'package:provider/provider.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 import 'package:simpoc/provider/sim_provider.dart';
 import 'package:simpoc/screens/landing_page.dart';
 import 'package:simpoc/service/sim_card_service.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await NoScreenshot.instance.screenshotOff();
+  await NoScreenshot.instance.screenshotWithImage();
   runApp(
     MultiProvider(
       providers: [
